c3ba74e467776e4995fe0b2f807352696ad1f1e2
silvioderechte09 <silviobasti@gmail.com>
2025-12-24 22:53:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import express from "express";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_, res) => res.send("Bot läuft"));
 app.listen(PORT, () => console.log("Webserver läuft"));
+
+const db = await open({
+  filename: "./whitelist.db",
+  driver: sqlite3.Database
+});
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
